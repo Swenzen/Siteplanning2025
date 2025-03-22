a33339e91aeb7a6671b6ca144ab0b14ce57c80e8
Pierre <magniepi@gmail.com>
2025-03-22 21:11:14 +0100

    k

--- routes/bdd/competences.js
+++ routes/bdd/competences.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const connection = require('../db'); // Assurez-vous que le chemin est correct
+const connection = require('../../db'); // Assurez-vous que le chemin est correct
 const authenticateToken = require('../middleware/auth'); // Importer le middleware d'authentification
 
 
