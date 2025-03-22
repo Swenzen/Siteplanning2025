8666e0112f227d47f59e8924238a368a8f5a0e43
Pierre <magniepi@gmail.com>
2025-03-22 11:57:12 +0100

    test creation user

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const pool = require('./db');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -86,6 +89,56 @@ app.get('/', (req, res) => {
 // Route de vérification de santé
 app.get('/health', (req, res) => {
     res.send('Application is running');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        // Hacher le mot de passe
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Insérer l'utilisateur dans la base de données
+        const query = 'INSERT INTO Tuser (username, password, email) VALUES (?, ?, ?)';
+        pool.query(query, [username, hashedPassword, email], (err, results) => {
+            if (err) {
+                console.error('Erreur lors de l\'inscription :', err.message);
+                return res.status(500).send('Erreur lors de l\'inscription');
+            }
+            res.status(201).send('Utilisateur inscrit avec succès');
+        });
+    } catch (err) {
+        console.error('Erreur lors du hachage du mot de passe :', err.message);
+        res.status(500).send('Erreur interne');
+    }
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const query = 'SELECT * FROM Tuser WHERE username = ?';
+    pool.query(query, [username], async (err, results) => {
+        if (err) {
+            console.error('Erreur lors de la connexion :', err.message);
+            return res.status(500).send('Erreur lors de la connexion');
+        }
+
+        if (results.length === 0) {
+            return res.status(401).send('Nom d\'utilisateur ou mot de passe incorrect');
+        }
+
+        const user = results[0];
+
+        // Vérifier le mot de passe
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).send('Nom d\'utilisateur ou mot de passe incorrect');
+        }
+
+        // Générer un jeton JWT
+        const token = jwt.sign({ userId: user.user_id }, 'votre_secret', { expiresIn: '1h' });
+        res.json({ token });
+    });
 });
 
 // Démarrer le serveur web
